da95e5d44f29ece26883a940932254e08dd1c03c
EbPeña <eberg.pena@gmail.com>
2020-10-17 08:33:45 -0500

    Se han agregado datos booleanos

---
+++ Tipo_de_datos .playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var estudios = "Grado Universitario"
+var salario = 0
+var cedulaProfesional = true
+var cedulaMaestria = false
+
+if cedulaProfesional {
+    salario = 9000
+}else{
+    salario = 8000
+}
+
+
+salario
