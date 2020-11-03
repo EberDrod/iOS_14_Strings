bae16ad159cf3d976b641cebd8d138f5759d03d1
Eber Peña <eberg.pena@gmail.com>
2020-11-02 18:58:52 -0600

    Se ejemplifico los operadores logicos

---
+++ Operadores de Operacion .playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+//Operadores de Operación
+var x = 1
+var y = 2
+
+//Igual a ==
+
+x == y
+
+//Diferente a !=
+
+x != y
+
+//Mayor que >
+
+x > y
+//Menor que <
+
+x < y
+
+//Mayor o igual que >=
+x >= y
+//Menor o igual que <=
+
+x <= y
+
