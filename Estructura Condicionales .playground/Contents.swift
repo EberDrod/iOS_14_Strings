206b7a4d149693bf466ca6b3347b248507801641
Eber Peña <eberg.pena@gmail.com>
2020-11-02 19:20:16 -0600

    ejemplo de asignación con operadores

---
+++ Estructura Condicionales .playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//Asignación de edad para dar acceso alguna situación
+var edad = 17
+var cantidadDinero = 1000
+var sexoFemenino = false
+
+    //true
+if !((edad >= 18 || cantidadDinero > 250) && (sexoFemenino)) {
+    
+    //false
+    print("Si puedes entrar al bar, ya que eres mayor de edad")
+    
+}else{
+    print("No puedes entrar al bar, ya que no eres mayor de edad ")
+    
+}
