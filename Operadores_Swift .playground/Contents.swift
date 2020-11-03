28280ba61b31e1af359efa7653ded00c4b2b89c6
Eber Peña <eberg.pena@gmail.com>
2020-11-02 18:52:50 -0600

    Se ejemplifica como manejar los operadores en Swift

---
+++ Operadores_Swift .playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+let numeroConstante = 18
+
+//Operadores Aritmeticos + - * /
+
+var numeroVariable = 2
+
+numeroConstante + numeroVariable
+numeroConstante - numeroVariable
+numeroConstante * numeroVariable
+numeroConstante  / numeroVariable
+
+//Operadores Compuestos
+
+//Numero Variable  = numeroVariable -2
+
+numeroVariable  += 2
+numeroVariable  -= 2
+numeroVariable  *= 2
+numeroVariable  /= 2
+
+
+
+
